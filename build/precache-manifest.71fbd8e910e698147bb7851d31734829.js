378c0f6e0228578d9cec32ed70e732a93cb0081e
Haris <hariskhan1435@gmail.com>
2020-10-17 22:52:01 +0500

    Bringing up demo

---
+++ build/precache-manifest.71fbd8e910e698147bb7851d31734829.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113a3ebbbd25796512755376dd89e157",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b350e8bb3fb449233a0a",
+    "url": "/static/css/main.611f39b5.chunk.css"
+  },
+  {
+    "revision": "83780c69b5c5574b9273",
+    "url": "/static/js/2.e752a839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e752a839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b350e8bb3fb449233a0a",
+    "url": "/static/js/main.bba319b5.chunk.js"
+  },
+  {
+    "revision": "b1820bfc6ade90caaf5b",
+    "url": "/static/js/runtime-main.51c4ed28.js"
+  }
+]);
